828c112345d841a2cc148eabb48c3c7612ccc28c
GGQ_sb1703 <gqguan@hotmail.com>
2019-08-06 15:13:01 +0800

    output the Seebeck coefficient, electrical resistance and thermal conductance of a thermocouple

--- TE_MaterialProp.m
+++ TE_MaterialProp.m
@@ -1,23 +1,31 @@
-function [ alpha, rho, kappa ] = TE_MaterialProp( T_avg )
+function [ alpha, R, K ] = TE_MaterialProp( T_avg, gf )
 %% calculate temperature dependent properties
 %   Notes of I/O arguments
 %   T_avg - (i double scalar) average temperature [K]
-%   alpha - (o double array, length(2)) Seebeck coefficient [V/K]
-%   rho   - (o double array, length(2)) electrical conductivity [ohm/m]
-%   kappa - (o double array, length(2)) thermal conductivity [W/m-K]
-%                            1: p-polar
-%                            2: n-polar
+%   gf    - (i double scalar) geometric factor
+%   alpha - (o double scalar) Seebeck coefficient [V/K]
+%   R     - (o double scalar) electrical conductance [ohm]
+%   K     - (o double scalar) thermal conductance [W/K]
 %
 %   by Dr. Guan Guoqiang @ SCUT on 2019-08-06
 %
 %%  function body
-%   parameters of property calculation
+% intialize
+results = zeros(0,3);
+% parameters of property calculation
 params = [2.2224e-5,  9.306e-7, -9.905e-10; ...
            5.112e-7,  1.634e-8,  6.279e-11; ...
            6.2605  , -2.777e-2,  4.131e-5 ];
+% temperature array
 Ts = [1, T_avg, T_avg^2];
-alpha = dot(params(1,:), Ts);
-rho   = dot(params(2,:), Ts);
-kappa = dot(params(3,:), Ts);
+% calculate the properties of thermocouple
+for i = 1:3
+    results(i) = dot(params(i,:), Ts); % results = [alpha rho kappa]
+end
+% pack the results
+alpha = (results(1)+results(1));
+R     = (results(2)+results(2))/gf;
+K     = (results(3)+results(3))*gf;
+%
 end
 
