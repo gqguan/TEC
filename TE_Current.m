172dab5bff1b14434208234e68481446fd0c9316
GGQ_sb1703 <gqguan@hotmail.com>
2019-08-07 15:49:33 +0800

    Add the calculation of one-stage TEC

--- TE_Current.m
+++ TE_Current.m
@@ -1,4 +1,4 @@
-function [I, Q] = TE_Current(Th, Tc, N, r, opt)
+function [I_calc] = TE_Current(Th, Tc, N, r, opt)
 %% Calculate current according hot- and cold-side temperatures
 %  notes of I/O arguments
 %  Th - (i double scalar) hot-side temperature [K]
@@ -6,58 +6,52 @@ function [I, Q] = TE_Current(Th, Tc, N, r, opt)
 %  N  - (i double scalar) number of thermocouples in the first stage
 %  r  - (i double scalar) number ratio of thermocouples in two stages
 %  opt- (i optional integer scalar) running mode
-%       0: (default) theoretical current to make E = I^2*R
-%       1: minimal current to make Qc = 0
-%  I  - (o double scalar) serial electric current flowing through the two
-%                         stages [A]
+%       0: (default) currents of one-stage TEC to make Qc = 0
+%       1:           currents of two-stage TEC to make Qc = 0
+%  I_calc - (o double array(2) for opt = 0) currents in one-stage TEC [A]
+%           (o double array(2) for opt = 1) currents in two-stage TEC [A]
 %  Q  - (o double array(2)) heats flowing out/in the hot/cold side of TEC
 %
 %  by Dr. Guan Guoqiang @ SCUT on 2019-08-06
 %
 %% function body
 % default argument of input opt
-if nargin < 5
+if nargin < 4
     opt = 0;
+    r = 0;
 end
+syms I;
 switch opt
     case 0
-        % initialize
-        I = 1;
-        dI = 1;
-        % 试差法计算电流
-        while dI > 1e-5
-            % 计算吸、放热量
-            [Q, ~, R, ~] = TE_Heat(Th, Tc, I, N, r);
-            % 计算输入电功
-            E = Q(1)-Q(2);
-            % 计算电流 
-            I_calc = sqrt(E/(R(1)*N+R(2)*N*r));
-            dI = abs(I-I_calc);
-            I = I_calc;
-        end
+        [a, R, K ] = TE_MaterialProp((Th+Tc)/2, 0.0015);
+        eq12 = 0 == I*a*Tc-I^2*R/2-K*(Th-Tc);
+        current = eval(solve(eq12, I));
+        I_calc = current;
     case 1
-        syms I;
-        % intialize
-        Tm = (Th+Tc)/2.d0;
-        dTm = 1e5;
-        while dTm > 1e-5
-            % 计算热电偶参数
-            % calculate a2 R2 K2
-            [a2, R2, K2] = TE_MaterialProp((Tc+Tm)/2, 0.0015);
-            % define heat balance equations
-            eq4 = 0 == (I*a2*Tc-I^2*R2/2-K2*(Tm-Tc))*N;
-            % 计算电流
-            current = eval(solve(eq4, I));
-            dTm_vec = zeros(size(current));
-            % 计算Tm
-            for i = 1:length(current)
-                dTm_vec(i) = TE_Tm(Th, Tc, current(i), r)-Tm;       
+        I_calc = [0 0];
+        for j = 1:2
+            % intialize
+            Tm = (Th+Tc)/2.d0;
+            dTm = 1e5;
+            while dTm > 1e-5
+                % 计算热电偶参数
+                % calculate a2 R2 K2
+                [a2, R2, K2] = TE_MaterialProp((Tc+Tm)/2, 0.0015);
+                % define heat balance equations
+                eq4 = 0 == (I*a2*Tc-I^2*R2/2-K2*(Tm-Tc))*N;
+                % 计算电流
+                current = eval(solve(eq4, I));
+                dTm_vec = zeros(size(current));
+                % 计算Tm
+                for i = 1:length(current)
+                    dTm_vec(i) = TE_Tm(Th, Tc, current(i), r)-Tm;       
+                end
+                dTm = dTm_vec(j);
+                Tm = Tm+dTm;
             end
-            [dTm, MinIdx] = min(abs(dTm_vec));
-            Tm = Tm+dTm_vec(MinIdx);
+            % 输出电流向量
+            I_calc(j) = current(j);
         end
-        % 输出电流向量
-        I = current;
     otherwise
         fprintf('[ERROR] Invalid input argument!\n');
         return
--- TE_Heat.m
+++ TE_Heat.m
@@ -7,18 +7,31 @@ function [Q, a, R, K] = TE_Heat(Th, Tc, I, N, r)
 %                         stages [A]
 %  N  - (i double scalar) number of thermocouples in the first stage
 %  r  - (i double scalar) number ratio of thermocouples in two stages
+%                         = 0 indicates one-stage TEC
 %  Q  - (o double array(2)) heats flowing out/in the hot/cold side of TEC
 %  a  - (o double array(2)) Seebeck coefficient of 1 and 2 stage of TEC
+%       (o double scalar)                          one-stage TEC
 %  R  - (o double array(2)) electrical resistance of 1 and 2 stage of TEC
+%       (o double scalar)                          one-stage TEC
 %  K  - (o double array(2)) thermal conductance of 1 and 2 stage of TEC
+%       (o double scalar)                          one-stage TEC
 %
 %  by Dr. Guan Guoqiang @ SCUT on 2019-08-06
 %
+%  2019-08-07: add case 0 for calculating the heats in one-stage TEC
+%
 %% function body
-% Get Tm
-[Tm, a, R, K] = TE_Tm(Th, Tc, I, r);
-% 吸、放热量
-Q(1) = (I*a(1)*Th+I^2*R(1)/2-K(1)*(Th-Tm))*N*r;
-Q(2) = (I*a(2)*Tc-I^2*R(2)/2-K(2)*(Tm-Tc))*N;
+switch r
+    case 0
+        [a, R, K] = TE_MaterialProp((Th+Tc)/2, 0.0015);
+        Q(1) = (I*a*Th+I^2*R/2-K*(Th-Tc))*N;
+        Q(2) = (I*a*Tc-I^2*R/2-K*(Th-Tc))*N;
+    otherwise
+        % Get Tm
+        [Tm, a, R, K] = TE_Tm(Th, Tc, I, r);
+        % 吸、放热量
+        Q(1) = (I*a(1)*Th+I^2*R(1)/2-K(1)*(Th-Tm))*N*r;
+        Q(2) = (I*a(2)*Tc-I^2*R(2)/2-K(2)*(Tm-Tc))*N;
+end
 %
 end
