db0ffd5a15b7a220ed5d4eec0091024048adea63
GGQ_sb1703 <gqguan@hotmail.com>
2019-08-09 13:53:35 +0800

    Add the feature to calculate the Qc and COP with given I

--- case1.m
+++ case1.m
@@ -6,15 +6,23 @@ clear;
 % set the waitbar
 ui_waitbar = waitbar(0, 'Setting ... ');
 % set parameters
-N = 127;
-Th = 300; 
-Tc = 250;
-r = 5;
+I_Set = 3.25;
+N = 190;
+Th = 50.6+273.15; 
+Tc = 16.5+273.15;
+r = 1;
+gf = 4.15e-4;
+N0 = N/(r+1);
 % 计算电流上下边界
-IBound = TE_Current(Th, Tc, N, r, 1);
+IBound = TE_Current(Th, Tc, N0, r, gf, 1);
 IMax = max(IBound);
 IMin = min(IBound);
-dI = (IMax-IMin)/100;
+dI = (IMax-IMin)/30;
+%
+if (I_Set>IMax || I_Set<IMin)
+    fprintf('Given electrical current is out of range!\n');
+    return;
+end
 % initialize
 I = IMin:dI:IMax;
 COP = zeros(size(I));
@@ -23,12 +31,19 @@ XCoordinate = zeros(size(I));
 waitbar(0, ui_waitbar, 'Calculating ... ')
 % fprintf('Calculating ... [%d]: ', length(I));
 for i = 1:length(I)
-    wb_str = sprintf('Calculating ... %4.1f %%', (i/length(I)*100));
-%     wb_str = ['Calculating ...', num2str(i/length(I)*100),'%'];
-    waitbar(i/length(I), ui_waitbar, wb_str)    
-    [Q, ~, ~, ~] = TE_Heat(Th, Tc, I(i), N, r);
+    str_wb = sprintf('Calculating ... %4.1f %%', (i/length(I)*100));
+    waitbar(i/length(I), ui_waitbar, str_wb)    
+    [Q, ~, ~, ~] = TE_Heat(Th, Tc, I(i), N0, r, gf);
     COP(i) = Q(2)/(Q(1)-Q(2));
-    XCoordinate(i) = Q(2)/N/(r+1);
+    XCoordinate(i) = Q(2)/N0/(r+1);
 end
+% 
+[Q, ~, ~, ~] = TE_Heat(Th, Tc, I_Set, N0, r, gf);
+COP_Set = Q(2)/(Q(1)-Q(2));
+XCoordinate_Set = Q(2)/N0/(r+1);
+%
 delete(ui_waitbar);
-fprintf(' completed \n');
+fprintf(' Complete calculation! \n');
+str_legend = sprintf('Th = %5.1f K, Tc = %5.1f K, r = %4.1f', Th, Tc, r);
+plot(XCoordinate, COP); legend(str_legend); hold on;
+plot(XCoordinate_Set, COP_Set, 'x');
