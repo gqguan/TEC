fea687ea4d89ddf1214b10fd62ed110c89aad6c8
GGQ_sb1703 <gqguan@hotmail.com>
2019-08-15 09:45:00 +0800

    DCMD module has divided into feed- and permeate-side CVs. Calculate the effluents of CVs with coupling material and energy conservation.

---
+++ DCMD_Diff_THTC.m
@@ -0,0 +1,54 @@
+function [fval] = DCMD_Diff_THTC(x, QH, QC, SInFeed, SInPerm, MembrProps)
+%% calculate the material and energy conservation
+%  notes of I/O arguments
+%  x    - (i real arrays(2)) x(1) = TH, mean temperature of feed-side
+%                                       channel in DCMD module [K]
+%                            x(2) = TC, mean temperature of permeate-side
+%                                       channel in DCMD module [K]
+%  QH   - (i real scalar) heat absorbed by feed from the external heat
+%                         source [W]
+%  QC   - (i real scalar) heat released from the permeate to the external
+%                         heat sink [W]
+%  SInFeed - (i struct) stream properties of feed-side influent
+%         .Temp: temperature [K]
+%         .MassFlow: mass flowrate [kg/s]
+%         .MassFraction: mass fraction of NaCl 
+%         .SpecHeat: specific heat [J/kg-K]
+%         .Enthalpy: enthalpy [W]
+%  SInPerm - (i struct) stream properties of permeate-side influent
+%         .Temp: temperature [K]
+%         .MassFlow: mass flowrate [kg/s]
+%         .MassFraction: mass fraction of NaCl 
+%         .SpecHeat: specific heat [J/kg-K]
+%         .Enthalpy: enthalpy [W]
+%  MembrProps - (i struct) variable of membrane properties
+%            .TMH: hot-side temperature of membrane [K]
+%            .TMC: cold-side temperature of membrane [K]
+%            .Area: effective area of membrane [m2]
+%            .Thickness: thickness of membrane [m]
+%            .MDCoefficient: MD coefficient [kg/s-m2-Pa]
+%            .ThermConductivity: thermal conductivity of membrane
+%  fval - (o real scalar) 
+%
+%  by Dr. Guan Guoqiang @ SCUT on 2019-08-10
+%  
+% initialize
+SFeedSide.Temp = x(1);
+SPermSide.Temp = x(2);
+SFeedSide.MassFraction = SInFeed.MassFraction;
+SPermSide.MassFraction = SInPerm.MassFraction;
+% feed-side heat and mass balance
+DirectOpt = -1;
+[QTransMembr,STransMembr] = DCMD_SPerm(DirectOpt, MembrProps, ...
+                                       SFeedSide, SPermSide);
+SOutFeed = DCMD_SOut(DirectOpt, SInFeed, STransMembr, QH, QTransMembr);
+% permeate-side heat and mass balance
+DirectOpt = 1;
+[QTransMembr,STransMembr] = DCMD_SPerm(DirectOpt, MembrProps, ...
+                                       SFeedSide, SPermSide);
+SOutPerm = DCMD_SOut(DirectOpt, SInPerm, STransMembr, QTransMembr, QC);
+% output
+xout = [SOutFeed.Temp,SOutPerm.Temp];
+fval = norm(xout-x);
+%
+end
---
+++ DCMD_SPerm.m
@@ -0,0 +1,71 @@
+function [QP,STransMembr] = DCMD_SPerm(DirectOpt, MembrProps, SFeedSide, SPermSide)
+%% calculate transmembrane permeation
+%  notes of I/O arguments
+%  DirectOpt  - (i integer scalar) opt = -1: flow into membrane
+%                                         1: flow out from membrane
+%  MembrProps - (i struct) variable of membrane properties
+%            .TMH: hot-side temperature of membrane [K]
+%            .TMC: cold-side temperature of membrane [K]
+%            .Area: effective area of membrane [m2]
+%            .Thickness: thickness of membrane [m]
+%            .MDCoefficient: MD coefficient [kg/s-m2-Pa]
+%            .ThermConductivity: thermal conductivity of membrane
+%  SFeedSide  - (i struct) properties of feed-side stream
+%           .Temp: temperature [K]
+%           .MassFlow: mass flowrate [kg/s]
+%           .Velocity: velocity [m/s]
+%           .MassFraction: mass fraction of NaCl
+%           .Density: density [kg/m3]
+%           .Viscosity: dynamic viscosity [Pa-s]
+%           .SpecHeat: specific heat [J/kg-K]
+%           .ThermCond: thermal conductivity [W/m-K]
+%           .Enthalpy: enthalpy [W]
+%  SPermSide  - (i struct) properties of permeate-side stream
+%           .Temp: temperature [K]
+%           .MassFlow: mass flowrate [kg/s]
+%           .Velocity: velocity [m/s]
+%           .MassFraction: mass fraction of NaCl
+%           .Density: density [kg/m3]
+%           .Viscosity: dynamic viscosity [Pa-s]
+%           .SpecHeat: specific heat [J/kg-K]
+%           .ThermCond: thermal conductivity [W/m-K]
+%           .Enthalpy: enthalpy [W]
+%  QTransMembr - (o real scalar) heat transfer across the membrane
+%  STransMembr - (o struct) variable of transmembrane stream
+%
+%  by Dr. Guan Guoqiang @ SCUT on 2019-08-13
+%
+%%
+% intialize
+C   = MembrProps.MDCoefficient;
+d   = MembrProps.Thickness;
+K   = MembrProps.ThermConductivity;
+TH  = SFeedSide.Temp;
+MF  = SFeedSide.MassFraction;
+TC  = SPermSide.Temp;
+% calculate the temperature at the membrane surface 
+TMH = TH; % *Under construction*
+TMC = TC; % *Under construction*
+% get permeation flux according to the difference of vapor pressure
+PSH = DCMD_SatVapPressure(TMH, MF);
+PSC = DCMD_SatVapPressure(TMC);
+JM  = C*(PSH-PSC);
+% get temperature-dependent latent heat
+dHv = DCMD_LatentHeat((TMH+TMC)/2);
+% calculate the transmembrane heat flux
+JH = JM*dHv+K/d*(TMH-TMC);
+% output
+switch DirectOpt
+    case -1
+        STransMembr.Temp = TMH;
+    case 1
+        STransMembr.Temp = TMC;
+end
+STransMembr.MassFlow = JM*MembrProps.Area;
+STransMembr.MassFraction = 0;
+STransMembr.SpecHeat = 4.18e3;
+STransMembr.Enthalpy = STransMembr.MassFlow * ...
+                       STransMembr.SpecHeat * ...
+                       STransMembr.Temp;
+QP = JH*MembrProps.Area;
+%
