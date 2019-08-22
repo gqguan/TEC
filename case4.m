be1918a20c5586b197ca71c3d9e74555b94757b4
GGQ_sb1703 <gqguan@hotmail.com>
2019-08-22 19:37:14 +0800

    Modify case4.m for 2-stage DCMD, however, the TS2H would be set arbitrarily.

--- case4.m
+++ case4.m
@@ -1,6 +1,7 @@
 %% Calculate temperatures of stacked DCMD module with TEHPs
+%  For the scenario of 2-stack DCMD integrated with 3 TEHPs
 %
-% by Dr. GUAN, Guoqiang @ SCUT on 2019-08-19
+%  by Dr. GUAN, Guoqiang @ SCUT on 2019-08-19
 %
 %% Initialize
 clear;
@@ -23,6 +24,7 @@ SInFeed = struct('Temp', 323.15,     ...
                  'SpecHeat', 4.18e3, ...
                  'ThermCond', 0.6,   ...
                  'Enthalpy', 0.015*4180*323.15);
+SInFeeds(1) = SInFeed; SInFeeds(2) = SInFeed;
 SInPerm = struct('Temp', 303.15,     ...
                  'MassFlow', 0.015,  ...
                  'Velocity', 0.015*1e-3/(0.04*0.006),  ...
@@ -32,6 +34,7 @@ SInPerm = struct('Temp', 303.15,     ...
                  'SpecHeat', 4.18e3, ...
                  'ThermCond', 0.6,   ...
                  'Enthalpy', 0.015*4180*303.15);
+SInPerms(1) = SInPerm; SInPerms(2) = SInPerm;
 %  MembrProps.TMH: hot-side temperature of membrane [K]
 %            .TMC: cold-side temperature of membrane [K]
 %            .Area: effective area of membrane [m2]
@@ -41,6 +44,7 @@ SInPerm = struct('Temp', 303.15,     ...
 MembrProps = struct('TMH', [], 'TMC', [], 'Area', 0.0016, ...
                     'Thickness', 1.5e-4, 'MDCoefficient', 3.2e-7, ...
                     'ThermConductivity', (0.18*0.3+0.025*0.7));
+Membranes(1) = MembrProps; Membranes(2) = MembrProps;
 %  TEC.NumTC     : Number of thermocouples in TEC
 %     .NumRatio  : ratio of thermocouples in the 1-stage TEC to those in
 %                  the 2-stage TEC
@@ -55,30 +59,33 @@ MembrProps = struct('TMH', [], 'TMC', [], 'Area', 0.0016, ...
 TEC = struct('NumTC', 190, 'NumRatio', 0, 'GeomFactor', 3.8e-4, ...
              'HTCoefficient', 270, 'HTArea', 0.0016, ...
              'SeebeckCoefficient', [], 'ElecConductance', [], ...
-             'ThermConductance', [], 'Voltage', 12, 'Current', 1);
-TECs(1) = TEC; TECs(2) = TEC;
-T0 = [322.1; 320.0; 303.6; 303.2];
-TE = [298.15; 307.7307];
-%% Solve temperatures
-fun = @(T)DCMD_EqSys(T, TE, TECs, SInFeed, SInPerm, MembrProps);
-[T, fvals, exitflag] = fsolve(fun, T0);
-%% Calculate the energy efficiency
-% COP of TECs
-COP = zeros(size(TECs));
-QH = zeros(size(TECs));
-QC = zeros(size(TECs));
-Q1 = TE_Heat(T(1), TE(1), TECs(1));
-QH(1) = Q1(1); QC(1) = Q1(2);
-COP(1) = QH(1)/(QH(1)-QC(1));
-Q2 = TE_Heat(TE(2), T(4), TECs(2));
-QH(2) = Q2(1); QC(2) = Q2(2);
-COP(2) = QC(2)/(QH(2)-QC(2));
-% Feed- and permeate-side effluents
-[~, SOutFeed] = DCMD_EqF(T(2), T(3), T(1), TE(1), TECs(1), ...
-                          SInFeed, SInPerm, MembrProps);
-[~, SOutPerm] = DCMD_EqF(T(2), T(3), TE(2), T(4), TECs(2), ...
-                          SInFeed, SInPerm, MembrProps);
-% Water permeation
-WF = SInFeed.MassFlow-SOutFeed.MassFlow;
-% Specific energy consumption
-SPC = (QH(1)-QC(1)+QH(2)-QC(2))/WF;
+             'ThermConductance', [], 'Voltage', 12, 'Current', 0.8);
+TECs(1) = TEC; TECs(2) = TEC; TECs(3) = TEC;
+T0 = 325; % initial values of TS2H
+TEXs = [298.15; 298.15]; % external heat source and sink temperature, 
+                         % i.e., [TS1C TS3H=307.7307]
+%% Solve coupling temperature TS2H
+fun = @(TS2H)DCMD_Diff_TS2H(TS2H, TEXs, TECs, SInFeeds, SInPerms, ...
+                            Membranes);
+TS2H = fsolve(fun, T0);
+%
+% %% Calculate the energy efficiency
+% % COP of TECs
+% COP = zeros(size(TECs));
+% QH = zeros(size(TECs));
+% QC = zeros(size(TECs));
+% Q1 = TE_Heat(T(1), TE(1), TECs(1));
+% QH(1) = Q1(1); QC(1) = Q1(2);
+% COP(1) = QH(1)/(QH(1)-QC(1));
+% Q2 = TE_Heat(TE(2), T(4), TECs(2));
+% QH(2) = Q2(1); QC(2) = Q2(2);
+% COP(2) = QC(2)/(QH(2)-QC(2));
+% % Feed- and permeate-side effluents
+% [~, SOutFeed] = DCMD_EqF(T(2), T(3), T(1), TE(1), TECs(1), ...
+%                           SInFeed, SInPerm, MembrProps);
+% [~, SOutPerm] = DCMD_EqF(T(2), T(3), TE(2), T(4), TECs(2), ...
+%                           SInFeed, SInPerm, MembrProps);
+% % Water permeation
+% WF = SInFeed.MassFlow-SOutFeed.MassFlow;
+% % Specific energy consumption
+% SPC = (QH(1)-QC(1)+QH(2)-QC(2))/WF;
