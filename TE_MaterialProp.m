b296f4d1aa72dc2dfa5a40ae07be5354cd5ee821
GGQ_sb1703 <gqguan@hotmail.com>
2020-03-28 11:08:43 +0800

    [document] 添加参考文献说明

--- TE_MaterialProp.m
+++ TE_MaterialProp.m
@@ -9,10 +9,13 @@ function [ alpha, R, K ] = TE_MaterialProp( T_avg, gf )
 %
 %   by Dr. Guan Guoqiang @ SCUT on 2019-08-06
 %
+%  References
+%  [1] Xuan X C, et al. Cryogenics, 2002, 42: 273-278.
+%
 %%  function body
 % intialize
 results = zeros(0,3);
-% parameters of property calculation
+% parameters of property calculation, ref. to eqs.(8)-(1) in [1]
 params = [2.2224e-5,  9.306e-7, -9.905e-10; ...
            5.112e-7,  1.634e-8,  6.279e-11; ...
            6.2605  , -2.777e-2,  4.131e-5 ];
@@ -22,7 +25,7 @@ Ts = [1, T_avg, T_avg^2];
 for i = 1:3
     results(i) = dot(params(i,:), Ts); % results = [alpha rho kappa]
 end
-% pack the results
+%% Êä³ö½á¹û
 alpha = (results(1)+results(1));
 R     = (results(2)+results(2))/gf;
 K     = (results(3)+results(3))*gf;
