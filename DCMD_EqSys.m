b3e17462a4cf121b83654cfea482921c076f2e19
GGQ_sb1703 <gqguan@hotmail.com>
2020-04-17 09:37:03 +0800

    [note] 补充说明过程计算

--- DCMD_EqSys.m
+++ DCMD_EqSys.m
@@ -1,7 +1,9 @@
 %% Define the equation system for coupling DCMD with TEHP
 %
 %  input arguments
-%  TIn- (real array) T(1)=TS1H, T(2)=T1H, T(3)=TM1H, T(4)=TM1C, T(5)=T1C,
+%  TIn- (real array(NumStage*6)) 长度为NumStage*6的温度向量，其中每级DCMD包括
+%  6个温度（依次分别为热侧壁面、热侧主体、热侧膜面、冷侧膜面、冷侧主体和冷侧壁面温度） 
+%                    T(1)=TS1H, T(2)=T1H, T(3)=TM1H, T(4)=TM1C, T(5)=T1C,
 %                    T(6)=TS2C, T(7)=TS2H, T(8)=T2H, T(9)=TM2H, 
 %                    T(10)=TM2C, T(11)=T2C, T(12) = TS3C
 %                    ...
@@ -51,69 +53,63 @@ QM        = zeros(size(Membranes));
 SOutFeeds = struct(SInFeeds);
 SOutPerms = struct(SInPerms);
 SM        = struct(SInPerms);
-T = reshape(TIn, [6,NumStack]);
+T = reshape(TIn, [6,NumStack]); % 将输入的温度向量重整为尺寸是[6,NumStack]的二维数组
 %% Define the equations for energy conservation
 for i = 1:NumStack
     % Boundary layer adhered the hot side of TECs(i)
+    % 计算该级膜组件冷热侧TEC的吸放热量
     if i == 1
         Q(i,:) = TE_Heat(T(1,i), TEXs(1), TECs(i));
     else
         Q(i,:) = TE_Heat(T(1,i), T(6,i-1), TECs(i));
     end
-    JHSH(i) = Q(i,1)/TECs(i).HTArea;
+    JHSH(i) = Q(i,1)/TECs(i).HTArea; % 从壁面传入热侧流体的热通量
+    % 计算热侧壁面与主流间的传热系数（W/m2-K）
     [~, hF(i)] = DCMD_TM(SInFeeds(i), -JHSH(i)); % negative JH1H indicates heat flowing into the feed
+    % 计算热侧壁面传热通量的差值，用于求解热侧壁面温度使壁面向主流传热通量与壁面流出热量相等
     fvals(1+(i-1)*6) = DCMD_HeatBalance_BL(T(1,i), T(2,i), JHSH(i), hF(i));
-    % Feed-side bulk flow of stage 1
+    % Feed-side bulk flow of stage i
+    % 初始化该级DCMD膜组件流出的冷热侧物流
     SOutFeeds(i) = SInFeeds(i); SOutPerms(i) = SInPerms(i);
+    % 设定冷热侧输出物流的温度
     SOutFeeds(i).Temp = T(2,i); SOutPerms(i).Temp = T(5,i);
+    % 重新计算物流性质
     SOutFeeds(i) = DCMD_PackStream(SOutFeeds(i));
     SOutPerms(i) = DCMD_PackStream(SOutPerms(i));
+    % 计算热侧流向膜面的热流和物流量
     [QM(i), SM(i)] = DCMD_Permeation(-1, Membranes(i), SOutFeeds(i), SOutPerms(i));
+    % 计算热侧主流CV中的能量差值，用于求解热侧主体温度使进出物流的焓变等于CV的净输入热量
     fvals(2+(i-1)*6) = DCMD_HeatBalance_BF(T(2,i), Q(i,1), QM(i), SInFeeds(i), SM(i));
-    % Boundary layer adhered the hot side of the membrane in the 1st stage
+    % Boundary layer adhered the hot side of the membrane in the i-th stage
+    % 计算热侧从主流向膜面的传热通量
     JHM(i) = QM(i)/Membranes(i).Area;
+    % 计算热侧主流与膜面传热通量的差值，用于求解热侧膜面温度使主流向膜面边界层流出的热量等于膜面边界层内的传热量
     fvals(3+(i-1)*6) = DCMD_HeatBalance_BL(T(2,i), T(3,i), JHM(i), hF(i));
     % Boundary layer adhered the cold side of the membrane in the 1st stage
+    % 计算冷侧从膜面流向主流的热流和物流量
     [QM(i), SM(i)] = DCMD_Permeation(1, Membranes(i), SOutFeeds(i), SOutPerms(i));
+    % 计算冷侧从膜面向主流的传热通量
     JHM(i) = QM(i)/Membranes(i).Area;
+    % 计算冷侧膜面边界层的传热系数
     [~, hP(i)] = DCMD_TM(SInPerms(i), JHM(i));
+    % 计算冷侧边界层中的净热量差，用于求解冷侧膜面温度使膜面向主流的传热量等于跨膜传热量
     fvals(4+(i-1)*6) = DCMD_HeatBalance_BL(T(4,i), T(5,i), JHM(i), hP(i));
-    % Permeate-side bulk flow of stage 2
+    % Permeate-side bulk flow of stage i+1
+    % 计算第i级DCMD中冷侧TEC（即第i+1级DCMD中的热侧TEC）壁面的吸放热量
     if i == NumStack
         Q(i+1,:) = TE_Heat(TEXs(2), T(6,i), TECs(i+1));
     else
         Q(i+1,:) = TE_Heat(T(1,i+1), T(6,i), TECs(i+1));
     end
+    % 计算第i级DCMD中冷侧壁面的吸热量
     JHSC(i+1) = Q(i+1,2)/TECs(i+1).HTArea;
+    % 计算冷侧主流CV的净热量差，用于求解冷侧主流温度使进出物流的焓变等于CV的净输入热量
     fvals(5+(i-1)*6) = DCMD_HeatBalance_BF(T(5,i), Q(i+1,2), QM(i), SInPerms(i), SM(i));
     % Boundary layer adhered the cold side of TEC2
+    % 计算冷侧壁面边界层的净热量差，用于求解冷侧壁面温度使边界层内传热量等于第i+1级DCMD热侧TEC的冷侧吸热量
     fvals(6+(i-1)*6) = DCMD_HeatBalance_BL(T(5,i), T(6,i), JHSC(i+1), hP(i));
 end
-%     % Boundary layer adhered the hot side of TEC2
-%     JHSH(2) = Q(2,1)/TECs(2).HTArea;
-%     [~, hF(2)] = DCMD_TM(SInFeeds(2), -JHSH(2)); % negative JH2H indicates heat flowing into the feed
-%     fvals(7) = DCMD_HeatBalance_BL(T(7), T(8), JHSH(2), hF(2));
-%     % Feed-side bulk flow of stage 2
-%     SOutFeeds(2) = SInFeeds(2); SOutPerms(2) = SInPerms(2);
-%     SOutFeeds(2).Temp = T(8); SOutPerms(2).Temp = T(11);
-%     SOutFeeds(2) = DCMD_PackStream(SOutFeeds(2));
-%     SOutPerms(2) = DCMD_PackStream(SOutPerms(2));
-%     [QM(2), SM(2)] = DCMD_Permeation(-1, Membranes(2), SOutFeeds(2), SOutPerms(2));
-%     fvals(8) = DCMD_HeatBalance_BF(T(8), Q(2,1), QM(2), SInFeeds(2), SM(2));
-%     % Boundary layer adhered the hot side of the membrane in the 2nd stage
-%     JHM(2) = QM(2)/Membranes(2).Area;
-%     fvals(9) = DCMD_HeatBalance_BL(T(8), T(9), JHM(2), hF(2));
-%     % Boundary layer adhered the cold side of the membrane in the 2nd stage
-%     [QM(2), SM(2)] = DCMD_Permeation(1, Membranes(2), SOutFeeds(2), SOutPerms(2));
-%     JHM(2) = QM(2)/Membranes(2).Area;
-%     [~, hP(2)] = DCMD_TM(SInPerms(2), JHM(2));
-%     fvals(10) = DCMD_HeatBalance_BL(T(10), T(11), JHM(2), hP(2));
-%     % Permeate-side bulk flow of stage 2
-%     Q(3,:) = TE_Heat(TEXs(2), T(12), TECs(3));
-%     JHSC(3) = Q(3,2)/TECs(3).HTArea;
-%     fvals(11) = DCMD_HeatBalance_BF(T(11), Q(3,2), QM(2), SInPerms(2), SM(2));
-%     % Boundary layer adhered the cold side of TEC3
-%     fvals(12) = DCMD_HeatBalance_BL(T(11), T(12), JHSC(3), hP(2));
+
 %% Output results
 F = fvals;
 end
