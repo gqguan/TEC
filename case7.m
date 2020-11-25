4b402725acbbfe3898d01b2b3d573ed08d903f97
PII_iNUC <gqguan@hotmail.com>
2020-11-26 02:02:53 +0800

    [add] 添加单级SFMD优化方案（模组件中温度过高）

---
+++ case7.m
@@ -0,0 +1,53 @@
+%% 多级集成热泵平板膜蒸馏单元的操作优化：一级SFMD最低单位能耗
+%
+% by Dr. Guan Guoqiang @ SCUT on 2020/11/25
+
+%% 初始化
+clear
+% 调用公用变量定义，其中包括DuctGeom（流道几何尺寸）、Stream（物料定义）、MembrProps（膜材料性质）
+CommonDef
+% 设定集成TEC多级SFMD系统的级数
+NumStage = 1;
+% 设定膜组件的热、冷侧进口温度和流率
+T1 = 323.15; T2 = 303.15;
+W1 = 1.5e-4; W2 = 1.5e-4;
+% 膜组件热侧进料初始化
+SInFeed = Stream;
+SInFeed.Temp = T1;
+SInFeed.MassFlow = W1;
+% calculate the rest properties of feed-side influent
+SInFeed = DCMD_PackStream(SInFeed);
+% 设定各级膜组件热侧进料
+SInFeeds(1:NumStage) = SInFeed;
+% 膜组件冷侧进料初始化
+SInPerm = Stream;
+SInPerm.Temp = T2;
+SInPerm.MassFlow = W2;
+% calculate the rest properties of permeate-side influent
+SInPerm = DCMD_PackStream(SInPerm);
+% 设定各级膜组件冷侧进料
+SInPerms(1:NumStage) = SInPerm;
+% 设定各级膜材料特性
+Membranes(1:NumStage) = MembrProps;
+% set properties for all TECs
+load('TEC_Params.mat') % 载入已有的TEC计算参数
+TECs(1:(NumStage+1)) = TEC_Params.TEC(3,1); % 注意按opt=0计算TEC的吸放热量
+I1 = TECs(1).Current;
+I2 = TECs(2).Current;
+
+%% 计算SFMD性能
+% 定义优化目标函数为单位能耗
+opfun = @(x)interface_SFMD(x, NumStage, SInFeeds, SInPerms, Membranes, TECs);
+% 初值
+x0 = [W1 T1 I1 W2 T2 I2];
+% 变量边界
+lb = [3.5e-6, 40+273.15, 0, 3.5e-6, 5+273.15, 0];
+ub = [2.1e-3, 80+273.15, 8, 2.1e-3, 30+273.15, 0];
+% 求优
+x = fmincon(opfun,x0,[],[],[],[],lb,ub);
+
+%% 输出结果
+[SEC,out1,out2] = opfun(x);
+format short g
+fprintf('Specific energy consumption of %d-stage DCMD is %.4e J/kg (%.4g kWh/kg).\n', NumStage, SEC, SEC*2.778e-7);
+disp(out1)
--- interface_SFMD.m
+++ interface_SFMD.m
@@ -1,33 +1,41 @@
 function [SEC, Output_Stages, Output_TEHP] = interface_SFMD(vars, NumStage, SInFeeds, SInPerms, Membranes, TECs)
 % vars - double array vars(1): W1
 %                     vars(2): T1
-%                     vars(3): W2
-%                     vars(4): T2
+%                     vars(3): I1
+%                     vars(4): W2
+%                     vars(5): T2
+%                     vars(6): I2
+%% 初始化
 if ~all([NumStage,length(SInFeeds),length(SInPerms),length(Membranes),length(TECs)-1])
     fprintf('[ERROR] 应输入单级SFMD的参数!\n')
     return
 end
+E = zeros(1,NumStage);
 % 载入共用变量
 CommonDef
 % 设置流率和温度
 SInFeeds(1).MassFlow = vars(1);
 SInFeeds(1).Temp = vars(2);
 SInFeeds = DCMD_PackStream(SInFeeds,DuctGeom);
-SInPerms(1).MassFlow = vars(3);
-SInPerms(1).Temp = vars(4);
+TECs(1).Current = vars(3);
+SInPerms(1).MassFlow = vars(4);
+SInPerms(1).Temp = vars(5);
 SInPerms = DCMD_PackStream(SInPerms,DuctGeom);
-% 计算产生率
+TECs(2).Current = vars(6);
+% 计算产水率、TEC能耗
 [~, Output_Stages, Output_TEHP] = SFMD(NumStage, SInFeeds, SInPerms, Membranes, TECs);
 WP = MembrProps(1).Area*Output_Stages.JM;
+E = Output_TEHP.EC;
+
 % 计算料液加热到设定温度的能耗
-Q1S = SInFeeds(1).MassFlow*SInFeeds(1).SpecHeat*(SInFeeds(1).Temp-298.15);
-E1 = Q1S*1; % 电加热100%效率的能耗
+QHS = SInFeeds(1).MassFlow*SInFeeds(1).SpecHeat*(SInFeeds(1).Temp-298.15);
+EH = QHS*1; % 电加热100%效率的能耗
 % 计算渗透液达到设定温度的能耗
-Q2S = SInPerms(1).MassFlow*SInPerms(1).SpecHeat*(SInPerms(1).Temp-298.15);
-if Q2S >= 0
-    E2 = Q2S; % 电加热100%效率的能耗
+QCS = SInPerms(1).MassFlow*SInPerms(1).SpecHeat*(SInPerms(1).Temp-298.15);
+if QCS >= 0
+    EC = QCS; % 电加热100%效率的能耗
 else
     eta_c = 1-SInPerms(1).Temp/298.15;
-    E2 = -Q2S*eta_c/(1-eta_c); % 逆向卡诺循环制冷的能耗
+    EC = -QCS*eta_c/(1-eta_c); % 逆向卡诺循环制冷的能耗
 end
-SEC = (E1+E2)/abs(WP);
+SEC = (EH+EC+sum(abs(E)))/abs(WP);
