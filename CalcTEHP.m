7fe4a0453b1d0f2a2a2d986172cd15f075f97fa4
pii-12700 <gqguan@hotmail.com>
2022-05-12 01:46:26 +0800

    [add] 按给定传热量计算TEC参数

---
+++ CalcTEHP.m
@@ -0,0 +1,67 @@
+%% 寻找DCMD膜组件中集成半导体热泵的设定参数，使TEC传热量满足指定值Q
+% 输入参数config为'feedTEHP'时，料液侧集成TEC从渗透液中吸热Q，加热膜组件中的料液
+%
+% by Dr. Guan Guoqiang @ SCUT on 2022/5/12
+
+function TECs = CalcTEHP(config,Q,sIn,TECs,TEXs,membrane,flowPattern,opts)
+
+    % 添加\Common目录以便调用自定义公用函数
+    homePath = cd;
+    idxPath = strfind(homePath,'\');
+    addpath([homePath(1:idxPath(2)),'Common\'])
+    % 日志文件
+    logFilename = [homePath,'\',datestr(datetime,'yyyymmdd'),'.log'];
+    logger = log4m.getLogger(logFilename);
+    logger.setFilename(logFilename);
+    % 函数测试用数据
+    if nargin == 0
+        [~,s1,membrane] = InitStruct();
+        T1 = 273.15+50; T2 = 273.15+45; % [K]
+        W1 = 1.217e-2; W2 = 6.389e-3; % [kg/s]
+        config = 'feedTEHP'; 
+        Q = 3.5; % 料液侧集成TEC的吸热量
+        s1.Temp = T1;
+        s1.MassFlow = W1;
+        sIn(1) = DCMD_PackStream(s1); % 膜组件料液侧进料
+        s2 = s1;
+        s2.Temp = T2;
+        s2.MassFlow = W2;
+        sIn(2) = DCMD_PackStream(s2); % 膜组件渗透侧进料
+        % set properties for all TECs
+        load('TEC_Params.mat','TEC_Params') % 载入已有的TEC计算参数
+        iTEC1 = 9;
+        TECs(1) = TEC_Params.TEC(iTEC1);
+        iTEC2 = 1; % 近似绝热
+        TECs(2) = TEC_Params.TEC(iTEC2); 
+        % 边界温度
+        TEXs(1) = T2; % 即TECs(1)的冷侧温度初设为渗透液进膜组件温度
+        TEXs(2) = 298.15;
+        % 流型
+        flowPattern = "countercurrent";
+        % TEC计算参数
+        opts = [TEC_Params.opt1(iTEC1),TEC_Params.opt2(iTEC1)];
+    end
+    % 参数
+    strIU = {'Current','Voltage'};
+    x0 = 1.2; lb = 0.5; ub = 10;
+    solOpts = optimoptions(@lsqnonlin,'Display','none');
+
+    switch config
+        case 'feedTEHP'
+            DiffQ2 = @(x)CalcQ2(x,sIn,TECs,TEXs,membrane,flowPattern,opts)-Q;
+            % 求TEC设定参数使min(TEC吸热量-指定值Q)
+            x1 = lsqnonlin(DiffQ2,x0,lb,ub,solOpts);
+            TECs(1).(strIU{opts(2)+1}) = x1;
+        case 'permTEHP'
+    end
+
+    function Q2 = CalcQ2(xVal,sIn,TECs,TEXs,membrane,flowPattern,opts)
+        % 根据opts(2)设定TECs(1)
+        TECs(1).(strIU{opts(2)+1}) = xVal;
+        % 计算集成半导体热泵DCMD膜组件中的侧形
+        [profile,~] = TEHPiDCMD(sIn,TECs,TEXs,membrane,flowPattern,opts);
+        % 料液侧集成半导体热泵的吸热量
+        Q2 = sum(cellfun(@(x)x(1,2),profile.QTEC));
+    end
+
+end
