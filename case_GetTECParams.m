ecd76211d89baa0e825e3ab5a6292eb14dc14857
GGQ_TP2101 <gqguan@hotmail.com>
2022-05-11 11:12:47 +0800

    [feature] 添加TEC设定方式选项opt2

--- case_GetTECParams.m
+++ case_GetTECParams.m
@@ -36,10 +36,11 @@ TE_ImportExpData
 %
 %% 优化
 % 命令行输入需要执行的优化方法
-opt = input(' 0 - Optimize (r g K) values according to https://doi.org/10.1016/S0011-2275(02)00035-8\n 1 - Optimize (a R K) values according to https://doi.org/10.1016/S0140-7007(99)00046-8\n Input 0 or 1 to select corresponding method to get the TEC parameters: ');
-TE_log(sprintf('Set opt = %d', opt))
+opt1 = input(' 0 - Optimize (r g K) values according to https://doi.org/10.1016/S0011-2275(02)00035-8\n 1 - Optimize (a R K) values according to https://doi.org/10.1016/S0140-7007(99)00046-8\n Input 0 or 1 to select corresponding method to get the TEC parameters: ');
+opt2 = input('TEC设定方式（输入0为电流、1为电压）：');
+TE_log(sprintf('Set opt1 = %d；opt2 = %d',opt1,opt2))
 % 设定优化向量的初值
-switch opt
+switch opt1
     case(0) % 优化r和g值，见参考文献[1]
         TE_log('Getting TEC type according to TEC.NumRatio');
         if TEC.NumRatio == 0
@@ -50,26 +51,26 @@ switch opt
             x0 = [TEC.NumRatio,TEC.GeomFactor,TEC.HTCoefficient];            
         end
     case(1) % 优化(a R K)值，见参考文献[2]
-        opt2a = input('Input polynomial order to correlate the (a R K) values: ');
-        TE_log(sprintf('Using %d-order polynomial function to correlate (a R K)', opt2a), 0, 1)
-        x0 = ones(3, opt2a+1);
-        x0(:,2:(opt2a+1)) = 0;
+        opt1a = input('Input polynomial order to correlate the (a R K) values: ');
+        TE_log(sprintf('Using %d-order polynomial function to correlate (a R K)', opt1a), 0, 1)
+        x0 = ones(3, opt1a+1);
+        x0(:,2:(opt1a+1)) = 0;
     otherwise
-        prompt = sprintf('Unknown running mode of %d in case_GetTECParams.m', opt);
+        prompt = sprintf('Unknown running mode of %d in case_GetTECParams.m', opt1);
         TE_log(prompt, 1);
         return
 end
 % 定义优化参数
 options = optimset('PlotFcns', @optimplotfval, 'MaxFunEvals', 1000*length(x0));
 % 定义目标函数
-fun = @(x)(TE_RMSE(x, TEC, ExpData, opt));
+fun = @(x)(TE_RMSE(x,TEC,ExpData,opt1,opt2));
 % 获得优化参数
 [x,fval,exitflag,optim_output] = fminsearch(fun, x0, options);
 TE_log(sprintf('RMSE(exp vs. sim) = %.3f', fval))
 switch exitflag
     case(1)
         TE_log(sprintf('Success to regress parameters after %d iterations', optim_output.iterations))
-        switch opt
+        switch opt1
             case(0)
                 TE_log('Check TEC properties for (r g k)')
             case(1)
@@ -86,7 +87,8 @@ end
 % 输入TEC部件号
 output.pid = input('Input TEC part no.: ', 's');
 % 参数优化方法
-output.opt = opt;
+output.opt1 = opt1;
+output.opt2 = opt2;
 saveOrNot = input('是否保存当前TEC参数到TEC_Params.mat：（输入1-保存，其他数值为放弃保存）');
 if saveOrNot == 1
     % 日志
@@ -101,6 +103,6 @@ if saveOrNot == 1
     else
         TEC_Params = current_tab;
     end
+    % 结果存盘
+    save('TEC_Params.mat', 'TEC_Params')
 end
-% 结果存盘
-save('TEC_Params.mat', 'TEC_Params')
