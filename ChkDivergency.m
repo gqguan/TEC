4cdd1f1a724b78238e44199f59e18b03c7636796
GGQ_TP2101 <gqguan@hotmail.com>
2022-05-13 20:14:12 +0800

    [note] 补充注释说明

--- ChkDivergency.m
+++ ChkDivergency.m
@@ -1,5 +1,5 @@
 %% 判定n次调用每次输入的数值是否增加
-% 功能说明：比较n次调用每次输入的数值，若数值每次比上一次输入数值更大则判定为发散
+% 功能说明：比较n次调用每次输入的数值，若数值每次比上一次输入数值相等或更大则判定为发散
 function outTF = ChkDivergency(inVal,n)
     if ~exist('n','var')
         n = 3;
@@ -10,13 +10,14 @@ function outTF = ChkDivergency(inVal,n)
         v = nan(1,n);
         v(1) = inVal;
     else
+        % 需要调用n次，采集n个数值；若调用次数小于n，将v数组中有元素为nan
         idx = arrayfun(@(x)isnan(x),v);
         if sum(idx) > 1
             v(1,find(idx,1)) = inVal;
         else
             if sum(idx) == 1
                 v(1,find(idx,1)) = inVal;
-            else
+            else % 已采集n个数据，则删除第一个元素后将输入数值添加到数组最后一个元素中
                 v(1) = []; v(n) = inVal;
             end
             [~,sortedI] = sort(v);
