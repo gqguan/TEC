cad1d6b1ac338093fe41f39aa7e896b3ffdf5e0c
pii-12700 <gqguan@hotmail.com>
2022-05-14 01:27:58 +0800

    [update] 绘制柱状图时自动提取results中的工艺方案

--- Postprocess.m
+++ Postprocess.m
@@ -2,13 +2,13 @@ function Postprocess(results,opStr)
 %% 后处理
 switch opStr
     case 'plotBar'
-        % 对比外置集成半导体热泵与传统DCMD的单位能耗
-        SEC0 = results.SEC(strcmp(results.CFG,'classical'));
-        SEC1 = results.SEC(strcmp(results.CFG,'extTEHP'));
-        bar([SEC0,SEC1])
+        % 对比集成半导体热泵与传统DCMD的单位能耗
+        cfgList = categories(categorical(results.CFG));
+        SEC = cell2mat(cellfun(@(x)results.SEC(strcmp(results.CFG,x)),cfgList','UniformOutput',false));
+        bar(SEC)
         xlabel('Case No.#')
         ylabel('SEC [kWh/kg]')
-        legend({'classical','extTEHP'})
+        legend(cfgList)
     case 'RSM'
         % 响应面分析
         factors = [results.W1,results.T1,results.W2,results.T2];
