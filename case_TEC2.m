6db570dc801afa5a435732ea1a9b5ef80c5799db
pii-12700 <gqguan@hotmail.com>
2022-05-15 17:49:21 +0800

    [feature] 添加绘制COP2(Th,Tc)曲面

--- case_TEC2.m
+++ case_TEC2.m
@@ -1,9 +1,9 @@
-%% 案例研究：绘制给定TEC在不同冷热侧温度下的吸放热量曲面
+%% 案例研究：绘制给定TEC在不同冷热侧温度下的吸放热量及制冷系数曲面
 %
 % by Dr. Guan Guoqiang @ SCUT on 2022-04-20
 
 %% 使用工作空间中的TEC参数
-if exist('TEC','var')
+if exist('TEC','var') && exist('opts','var')
     fprintf('使用当前工作空间中的TEC参数\n')
 else
     error('未在当前工作空间中找到变量TEC！')
@@ -18,20 +18,24 @@ Vset = 9;
 % 计算TEC吸放热量
 Q1 = zeros(length(TCs),length(THs));
 Q2 = zeros(size(Q1));
+E = zeros(size(Q1));
+COP2 = zeros(size(Q1));
 for i = 1:length(TCs)
     for j = 1:length(THs)
         Tc = TCs(i);
         Th = THs(j);
-        Q = TE_Heat(Th,Tc,TEC,0,1);
+        Q = TE_Heat(Th,Tc,TEC,opts(1),opts(2));
         Q1(i,j) = Q(1);
         Q2(i,j) = Q(2);
+        E(i,j) = Q(1)-Q(2);
+        COP2(i,j) = Q2(i,j)/E(i,j);
     end
 end
 
 %% 输出
 [X,Y] = meshgrid(TCs,THs);
-surf(X,Y,Q1)
+surf(X,Y,COP2)
 xlabel('T_C [K]')
 ylabel('T_H [K]')
-zlabel('Q_1 [W]')
+zlabel('COP_2')
 title(sprintf('U=%.1f V',Vset))
