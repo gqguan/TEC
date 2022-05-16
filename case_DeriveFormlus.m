e08d1c724937f976a190becf5baaf82a53c70001
pii-12700 <gqguan@hotmail.com>
2022-05-16 09:46:25 +0800

    [feature] 计算feedTEHP和permTEHP的WF和Q1

--- case_DeriveFormlus.m
+++ case_DeriveFormlus.m
@@ -11,10 +11,11 @@ syms W1 TF1 R % 操作参数
 syms WP QM TMF TF2 % 模拟求得参数
 syms cp1 % 物性参数
 syms T0 % 环境参数
-% 对于笔记2022/5/6所示系统
+% 对于笔记2022/5/6所示系统（classical、extTEHP和permTEHP）
 % 对于回流混合单元CV1，能量平衡方程
 WR1 = R*(WF-WP);
 cv1EEq = WF*cp1*T0 + WR1*cp1*TF2 == W1*cp1*TF0;
+cv1aEEq = WF*cp1*T0 + WR1*cp1*TF2 == W1*cp1*TF1;
 % 对于DCMD料液侧CV2，能量平衡方程（即为系统内每个单元都进行能量平衡综合的结果）
 cv2EEq = WF*cp1*T0 + Q1 == (WF-WP)*cp1*TF2+WP*cp1*TMF+QM;
 % 对于料液加热单元，能量平衡方程
@@ -23,9 +24,21 @@ cv3EEq = W1*cp1*TF0 + Q1 == W1*cp1*TF1;
 cv4EEq = W1*cp1*TF1 == (W1-WP)*cp1*TF2+WP*cp1*TMF+QM;
 % 联立求解
 sol1 = solve([cv1EEq,cv2EEq,cv3EEq],[WF,TF0,Q1]);
+sol1a = solve([cv1aEEq,cv2EEq],[WF,Q1]);
 % 显示结果
+disp('对于classical和extTEHP')
+disp('WF = ')
+disp(sol1.WF)
+disp('TF0 = ')
+disp(sol1.TF0)
 disp('Q1 = ')
 disp(sol1.Q1)
+
+disp('对于feedTEHP和permTEHP1')
+disp('WF = ')
+disp(sol1a.WF)
+disp('Q1 = ')
+disp(sol1a.Q1)
 
 %% 稳态操作时给定DCMD系统料液侧吸热量计算回流比及料液处理量
 % 联立求解
