6f7ceadda39e2c10ba4f8a9cb72b9e7b608bdc0d
GGQ_TP2101 <gqguan@hotmail.com>
2022-06-13 11:00:58 +0800

    [feature] 模拟给定系统功耗和冷热侧循环流率的外置集成半导体热泵DCMD系统，其中料液处理量根据稳态操作下的物热平衡计算

--- SimDCMD3.m
+++ SimDCMD3.m
@@ -1,4 +1,4 @@
-function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
+function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset,W1,W2,T0)
 % 模拟给定功耗的外置集成半导体热泵DCMD系统
     % 初始化
     strIU = {'Current','Voltage'};
@@ -18,8 +18,12 @@ function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
         s2.Temp = T2;
         s2.MassFlow = W2;
         stream.R2 = DCMD_PackStream(s2); % 膜组件渗透侧进料
+        % 设定系统操作参数
+        Eset = 24; % TEC功耗
+        WF = 1.5e-5;
+        T0 = 298.15;
+        % 设定半导体热泵参数
         iTEC1 = 22;
-        % set properties for all TECs
         load('TEC_Params.mat','TEC_Params') % 载入已有的TEC计算参数
         extTEC = TEC_Params.TEC(iTEC1);
         opts = [TEC_Params.opt1(iTEC1),TEC_Params.opt2(iTEC1)];
@@ -29,23 +33,26 @@ function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
         TEXs(1) = 298.15; % 膜组件的环境温度
         TEXs(2) = 298.15;
         membrane = MembrProps; % 膜性质
-        % 设定系统操作参数
-        Eset = 12; % TEC功耗
-        refluxRatio = inf; % 回流比
     end
-    % 计算外置集成半导体热泵的热、冷两侧进口流率和温度
-%     x0 = [W1,T1+15,W2,T2+5];
-%     solOpt = optimoptions(@lsqnonlin,'Display','iter',...
-%                                  'Algorithm','levenberg-marquardt');
-%     x1 = lsqnonlin(@extTEHP,x0,[],[],solOpt);
-    % 迭代求解[stmF0,stmP2]
+    % 系统进料（stream.F）初值，变量stream.F会在后续迭代中修正
+    stream.F = Stream;
+    stream.F.Temp = T0;
+    stream.F.MassFlow = WF;
+    stream.F = DCMD_PackStream(stream.F);
+    % 迭代求解[TF0,TP2]
+    stream.F0.MassFlow = W1; stream.R2.MassFlow = W2;
+    stream.F0 = DCMD_PackStream(stream.F0);
+    stream.R2 = DCMD_PackStream(stream.R2);
+
+    TF0 = mean([T1,T2]); TP2 = T2;
     for iter = 1:1000 % 最大迭代次数为1000
-        [stmF0,stmR2] = extTEHP(stream.F0,stream.R2);
-        if IsConvergence([stmF0,stmR2],[stream.F0,stream.R2])
+        [TF0a,TP2a] = extTEHP(TF0,TP2); 
+        disp([TF0a,TP2a])
+        if IsConvergence([TF0a,TF0],[TP2a,TP2])
             break
         else
-            stream.F0 = stmF0;
-            stream.R2 = stmR2;
+            TF0 = TF0a;
+            TP2 = TP2a;
         end
     end
     if iter == 1000
@@ -62,8 +69,11 @@ function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
     end
     disp(outTab)
     
-    function [stmF0,stmR2] = extTEHP(F0,R2)
-        stream.F0 = F0; stream.R2 = R2;
+    function [TF0a,TP2a] = extTEHP(TF0,TP2)
+        stream.F0.Temp = TF0; stream.R2.Temp = TP2;
+%         stream.F0.MassFlow = W1; stream.R2.MassFlow = W2;
+        stream.F0 = DCMD_PackStream(stream.F0);
+        stream.R2 = DCMD_PackStream(stream.R2);
         [SOUTs,QTEC] = HXiTEHP([stream.F0,stream.R2],extTEC,opts,Eset);
         stream.F1 = SOUTs(1);
         stream.P1 = SOUTs(2);
@@ -71,17 +81,29 @@ function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
         [profile,SOUTs] = TEHPiDCMD1(SINs,TECs,TEXs,membrane,flowPattern,opts);
         stream.F2 = SOUTs(1);
         stream.P2 = SOUTs(2);
-        stmR2 = CalcR2(stream.P1,stream.P2);
+        TP2a = stream.P2.Temp;
         stream.P = SPLIT(stream.P2,stream.R2);
-        [stream.FP,stream.R1] = SPLIT(stream.F2,refluxRatio);
-        stream.F = CalcFeed(stream.P,stream.FP);
+        [stream.FP,stream.F] = CalcFP(stream.F2,stream.P,-diff(QTEC));
+        stream.R1 = SPLIT(stream.F2,stream.FP);        
         stmF0 = MIX(stream.R1,stream.F);
-%         y(1) = (stmF0.MassFlow-x(1))/x(1); 
-%         y(2) = (stmF0.Temp-x(2))/x(2);
-%         y(3) = (stmR2.MassFlow-x(3))/x(3); 
-%         y(4) = (stmR2.Temp-x(4))/x(4);
+        TF0a = stmF0.Temp;
     end
     
+    function [stmFP,stmF,msg] = CalcFP(stmF2,stmP,E)
+        stmFP = stmF2; stmF = stream.F; msg = ''; % 设初值
+        % 联立HFP=E+HF-HP、HF=WF*cp*T0和HFP=(WF-WP)*cp*TF2求得WF
+        stmF.MassFlow = (E-stmP.Enthalpy+stmP.MassFlow*stmF2.SpecHeat*stmF2.Temp)/stream.F.SpecHeat/(stmF2.Temp-stmF.Temp);
+        stmF = DCMD_PackStream(stmF); % 维持进料温度为T0，计算stmF
+        % 计算料液侧出料
+        stmFP.MassFlow = stmF.MassFlow-stmP.MassFlow;
+        stmFP.Temp = stmF2.Temp;
+        stmFP = DCMD_PackStream(stmFP);
+        dHB = E+stmF.Enthalpy-stmP.Enthalpy-stmFP.Enthalpy; % 系统能量平衡偏差
+        if dHB > 1e-6
+            msg = sprintf('【注意】CalcFP()能量平衡偏差%.5g！',dHB);
+        end
+    end
+
     function stmR2 = CalcR2(stmP1,stmP2)
         stmR2 = stmP2;
         stmR2.MassFlow = stmP1.MassFlow;
@@ -119,19 +141,27 @@ function [stream,QTEC,profile,exitflag] = SimDCMD3(Eset)
 
     function result = IsConvergence(sins1,sins2,tol)
         if ~exist('tol','var')
-            tol = 1e-6;
+            tol = 1e-5;
         end
-        vec1a = arrayfun(@(x)x.MassFlow,sins1);
-        vec1a = vec1a/mean(vec1a);
-        vec1b = arrayfun(@(x)x.Temp,sins1);
-        vec1b = vec1b/mean(vec1b);
-        vec1 = [vec1a,vec1b];
-        vec2a = arrayfun(@(x)x.MassFlow,sins2);
-        vec2a = vec2a/mean(vec2a);
-        vec2b = arrayfun(@(x)x.Temp,sins2);
-        vec2b = vec2b/mean(vec2b);
-        vec2 = [vec2a,vec2b];
-        d = norm(vec1-vec2); disp(d)
+        if isstruct(sins1)
+            vec1a = arrayfun(@(x)x.MassFlow,sins1);
+            vec1a = vec1a/mean(vec1a);
+            vec1b = arrayfun(@(x)x.Temp,sins1);
+            vec1b = vec1b/mean(vec1b);
+            vec1 = [diff(vec1a),diff(vec1b)];
+        else
+            vec1 = diff(sins1);
+        end
+        if isstruct(sins2)
+            vec2a = arrayfun(@(x)x.MassFlow,sins2);
+            vec2a = vec2a/mean(vec2a);
+            vec2b = arrayfun(@(x)x.Temp,sins2);
+            vec2b = vec2b/mean(vec2b);
+            vec2 = [diff(vec2a),diff(vec2b)];
+        else
+            vec2 = diff(sins2);
+        end
+        d = norm([vec1,vec2]); % disp(d)
         result = (d<tol);
     end
 
